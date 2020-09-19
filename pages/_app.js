10e8a95faf723665bb8a1ad9000aed5b7137c2a8
Taras Samoilenko <taras.samoilenko@pindify.com>
2020-09-19 22:10:13 +0300

    added prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { AuthProvider } from '../lib/auth'
+import { AuthProvider } from '../lib/auth';
 
 const App = ({ Component, pageProps }) => {
   return (
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
-  )
-}
-export default App
+  );
+};
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { useAuth } from '../lib/auth';
+
 export default function Index() {
   const auth = useAuth();
+
   return auth.user ? (
     <div>
       <p>Email: {auth.user.email}</p>
