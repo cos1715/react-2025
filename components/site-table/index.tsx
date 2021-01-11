4188158608b9eccbf55f15f545b84db2c218fa4d
Taras Samoilenko <tsamoilenko@surprisehr.com>
2021-01-11 21:22:23 +0200

    as removed

--- components/site-table/index.tsx
+++ components/site-table/index.tsx
@@ -27,11 +27,7 @@ export const SiteTable: React.FC<IProps> = ({ sites }) => {
           {sites.map((site, index) => (
             <Box as="tr" key={site.id}>
               <Td>
-                <NextLink
-                  href="/sites/[siteId]"
-                  as={`/sites/${site.id}`}
-                  passHref
-                >
+                <NextLink href="/sites/[siteId]" passHref>
                   <Link id={`site-table-link-${index}`} fontWeight="medium">
                     {site.name}
                   </Link>
@@ -43,20 +39,14 @@ export const SiteTable: React.FC<IProps> = ({ sites }) => {
                 </Link>
               </Td>
               <Td>
-                <NextLink
-                  href="/sites/[siteId]"
-                  as={`/sites/${site.id}`}
-                  passHref
-                >
+                <NextLink href="/sites/[siteId]" passHref>
                   <Link color="blue.500" fontWeight="medium">
                     View Feedback
                   </Link>
                 </NextLink>
               </Td>
               <Td>{format(parseISO(site.createdAt), 'PPpp')}</Td>
-              <Td>
-                {/* <DeleteSiteButton siteId={site.id} /> */}
-              </Td>
+              <Td>{/* <DeleteSiteButton siteId={site.id} /> */}</Td>
             </Box>
           ))}
         </tbody>
